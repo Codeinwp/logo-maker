deee2de91b6243ff1b70b932fd1cd162ca2f9da8
Pedro <soarerobert17@gmail.com>
2020-11-25 14:18:41 +0000

    add set 4

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -1,6 +1,7 @@
 import set1 from "./set1/index"
 import set2 from "./set2/index"
 import set3 from "./set3/index"
+import set4 from "./set4/index"
 
 export type LogoSVGImport = {
     id: string
@@ -13,4 +14,4 @@ export type LogoSVGImport = {
 
 // Export a list with all the logos
 // Useful for rendering a select input or a preview with all the logos available
-export default [...set1, ...set2, ...set3]
+export default [...set1, ...set2, ...set3, ...set4]
---
+++ src/assets/logos/set4/index.ts
@@ -0,0 +1,19 @@
+import Logo31 from "./logo31"
+import Logo32 from "./logo32"
+import Logo33 from "./logo33"
+import Logo34 from "./logo34"
+import Logo35 from "./logo35"
+import Logo36 from "./logo36"
+import Logo37 from "./logo37"
+import Logo38 from "./logo38"
+import Logo39 from "./logo39"
+import Logo40 from "./logo40"
+
+// Export a obejct with all the logos
+// Useful for importing only some specific logos
+export { Logo31, Logo32, Logo33, Logo34, Logo35, Logo36, Logo37, Logo38, Logo39, Logo40 }
+
+
+// Export a list with all the logos
+// Useful for rendering a select input or a preview with all the logos available
+export default [Logo31, Logo32, Logo33, Logo34, Logo35, Logo36, Logo37, Logo38, Logo39, Logo40]
