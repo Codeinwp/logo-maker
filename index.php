267a358d84612f5c64bd4d81de6591e823cb4424
S. Robert <soarerobert17@gmail.com>
2021-05-10 15:24:54 +0000

    Update version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Logo Maker WordPress Plugin
  * Plugin URI: https://github.com/Codeinwp/logo-maker
  * Description: Logo Maker as plugin for WordPress
- * Version: 1.9.1
+ * Version: 1.10.0
  * Author: Soare Robert Daniel
  *
  * @package logo-maker
