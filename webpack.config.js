cab3125f124b58cec22d57f2f0781be8f31c4f18
Robert <soarerobert17@gmail.com>
2022-03-02 15:24:41 +0200

    Use dev build

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const path = require("path");
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 // const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin")
 // const webpackFonts = require("./webpack.fonts.json")
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
 	...defaultConfig,
+	devtool: 'development' === NODE_ENV ? 'eval-source-map' : undefined,
+	mode: NODE_ENV,
 	entry: {
 		"index": "./wordpress/index.tsx",
 		"logo-maker": "./src/index.tsx"
